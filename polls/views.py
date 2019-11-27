c9c9c2cb5f1ff31c400e1ec8bcd596667db33d58
Svetlozar Stoyanov <svetlozar.stoyanov@gmail.com>
2019-11-28 00:06:15 +0200

    Part3 - Use shortcut get_object_or_404.

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render
-from django.http import HttpResponse, Http404
+from django.shortcuts import get_object_or_404, render
 
 from .models import Question
 
@@ -12,7 +11,7 @@ def detail(request, question_id):
     try:
         question = Question.objects.get(pk=question_id)
     except Question.DoesNotExist:
-        raise Http404("Question does not exist")
+        raise get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
