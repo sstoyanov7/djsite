03b814b0b16d4aa7ac4fbc6a4d1d5646511fb27c
Svetlozar Stoyanov <svetlozar.stoyanov@gmail.com>
2019-11-30 00:46:16 +0200

    Part5 - Add QuestionDetailViewTests.
    Signed-off-by: Svetlozar Stoyanov <svetlozar.stoyanov@gmail.com>

--- polls/tests.py
+++ polls/tests.py
@@ -67,6 +67,16 @@ class QuestionIndexViewTests(TestCase):
             ['<Question: Past question.>']
         )
 
+    def test_past_question2(self):
+        """
+        The detail view of a question with a pub_date in the past
+        displays the question's text.
+        """
+        past_question = create_question(question_text='Past Question.', days=-5)
+        url = reverse('polls:detail', args=(past_question.id,))
+        response = self.client.get(url)
+        self.assertContains(response, past_question.question_text)
+
     def test_future_question(self):
         """
         Questions with a pub_date in the future aren't displayed on
@@ -101,6 +111,17 @@ class QuestionIndexViewTests(TestCase):
             response.context['latest_question_list'],
             ['<Question: Past question 2.>', '<Question: Past question 1.>']
         )
+
+class QuestionDetailViewTests(TestCase):
+    def test_future_question(self):
+        """
+        The detail view of a question with a pub_date in the future
+        returns a 404 not found.
+        """
+        future_question = create_question(question_text='Future question.', days=5)
+        url = reverse('polls:detail', args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
 
     def test_past_question(self):
         """
