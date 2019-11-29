dac9bea8247df6b9b316730725bc50693e390e57
Svetlozar Stoyanov <svetlozar.stoyanov@gmail.com>
2019-11-30 00:21:16 +0200

    Part5 - Added QuestionIndexViewTests and update get_queryset()

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
-from django.urls import reverse
+from django.urls import reverse, timezone
 from django.views import generic
 
 from .models import Choice, Question
@@ -11,7 +11,9 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        return  Question.objects.filter(
+        pub_date__lte=timezone.now()
+    ).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
