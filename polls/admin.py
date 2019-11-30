f1029de6f166ad1915963d207a7955b100e1cd2c
Svetlozar Stoyanov <svetlozar.stoyanov@gmail.com>
2019-11-30 11:09:15 +0200

    Part7 - Add labels to columns in Admin > Questions.
    Signed-off-by: Svetlozar Stoyanov <svetlozar.stoyanov@gmail.com>

--- polls/admin.py
+++ polls/admin.py
@@ -12,6 +12,7 @@ class QuestionAdmin(admin.ModelAdmin):
         (None,               {'fields': ['question_text']}),
         ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
     inlines = [ChoiceInline]
 
 admin.site.register(Question, QuestionAdmin)
